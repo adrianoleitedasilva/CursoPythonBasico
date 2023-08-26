cb16d8f4a8abc7120283868e939dc265db5d8749
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2023-08-26 15:13:40 -0300

    Create 01_vars.py

---
+++ Exemplos/01_vars.py
@@ -0,0 +1,17 @@
+
+# Números inteiros
+a = 10
+b = 11
+salario = 4000
+
+# Números com pontos flutuantes
+precoUnitario = 1.99
+
+# Booleanos
+vaiChover = True
+eleRiu = False
+
+# Strings
+nome = "Adriano"
+sobrenome = "Silva"
+email = 'adrianoleitedasilva@outlook.com'
